3df0136b6985ee401edc6f48b944cc2397c52a3a
Anıl Kul <mehmetanilkul@gmail.com>
2020-01-19 12:33:52 +0300

    Initial commit

---
+++ LoodosMovieClub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoodosMovieClub
+//
+//  Created by Mehmet Anıl Kul on 19.01.2020.
+//  Copyright © 2020 Mehmet Anıl Kul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
