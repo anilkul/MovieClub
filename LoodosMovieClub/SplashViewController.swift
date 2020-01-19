ee3ac305d259bc3e40379f43c940e96dce58722d
Anıl Kul <mehmetanilkul@gmail.com>
2020-01-19 12:53:01 +0300

    Add splash screen ui

--- LoodosMovieClub/SplashViewController.swift
+++ LoodosMovieClub/SplashViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashViewController.swift
 //  LoodosMovieClub
 //
 //  Created by Mehmet Anıl Kul on 19.01.2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
