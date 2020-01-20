c079ec36580ee43197b5df45f1ae5f65898998a3
Anıl Kul <mehmetanilkul@gmail.com>
2020-01-20 18:41:58 +0300

    Add variables and IBOutlets to detail view controller and detail view model

--- LoodosMovieClub/Screens/Detail Screen/View Model/DetailViewModel.swift
+++ LoodosMovieClub/Screens/Detail Screen/View Model/DetailViewModel.swift
@@ -11,14 +11,65 @@ import Foundation
 protocol DetailViewModelProtocol {
   var imdbID: String { get set }
   var networkManager: NetworkManagerProtocol { get set }
+  
+  var title: String { get set }
+  var posterUrlString: String { get set }
+  var genre: String { get set }
+  var rated: String { get set }
+  var type: String { get set }
+  
+  // var ratings
+  
+  var plot: String { get set }
+  var director: String { get set }
+  var writer: String { get set }
+  var actors: String { get set }
 }
 
 class DetailViewModel: DetailViewModelProtocol {
+  var title: String
+  var posterUrlString: String
+  var genre: String
+  
+  var rated: String
+  var type: String
+  var plot: String
+  
+  var director: String
+  var writer: String
+  var actors: String
+  
   var imdbID: String
   var networkManager: NetworkManagerProtocol
   
   init(imdbID: String, networkManager: NetworkManagerProtocol) {
+    title = Constants.Strings.emptyString
+    posterUrlString = Constants.Strings.emptyString
+    genre = Constants.Strings.emptyString
+    
+    rated = Constants.Strings.emptyString
+    type = Constants.Strings.emptyString
+    plot = Constants.Strings.emptyString
+    
+    director = Constants.Strings.emptyString
+    writer = Constants.Strings.emptyString
+    actors = Constants.Strings.emptyString
+    
     self.imdbID = imdbID
     self.networkManager = networkManager
+    
+    fetchMovieDetail(for: imdbID)
+  }
+  
+  private func fetchMovieDetail(for imdbID: String) {
+    
+  }
+  
+  private func parseMovieDetail() {
+    
+  }
+  
+  private func configureMovieDetail() {
+    
   }
 }
--- LoodosMovieClub/Screens/Detail Screen/View/View Controllers/DetailViewController.swift
+++ LoodosMovieClub/Screens/Detail Screen/View/View Controllers/DetailViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class DetailViewController: UIViewController {
   
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var posterImageView: UIImageView!
+  @IBOutlet weak var genreLabel: UILabel!
+  @IBOutlet weak var ratedLabel: UIImageView!
+  @IBOutlet weak var typeLabel: UIImageView!
+  
+  @IBOutlet weak var imdbLabel: UILabel!
+  @IBOutlet weak var rottenTomatoesLabel: UILabel!
+  @IBOutlet weak var metacriticLabel: UILabel!
+  
+  @IBOutlet weak var plotLabel: UILabel!
+  @IBOutlet weak var directorLabel: UILabel!
+  @IBOutlet weak var writerLabel: UILabel!
+  @IBOutlet weak var actorsLabel: UILabel!
+  
   var viewModel: DetailViewModel!
   
   override func viewDidLoad() {
